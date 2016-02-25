50479a6b9ebd4856a1940bd7521c91bbdf67f1a4
Riccardo Tacconi <rtacconi@smart421.com>
2016-02-25 21:12:23 +0000

    added ack as dependency

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://supermarket.chef.io"
 
 metadata
+
+cookbook 'ack', '~> 1.0.2'
--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures testhome'
 long_description 'Installs/Configures testhome'
 version          '0.1.0'
+
+depends 'ack'
